d3ac57505abc9fc1374c7a685299dcd16a62f52e
Rafael Carvalho <>
2018-05-29 10:59:09 -0300

    Add new locators and add logout function

--- pages/metamaskPage.js
+++ pages/metamaskPage.js
@@ -13,11 +13,21 @@ const MetamaskPage = function () {
 	this.createButton = element(by.buttonText('Create'));
 	this.vaulCreatedTextBox = element(by.className('twelve-word-phrase'));
 	this.atrLink = element(by.css('#app-content > div > div.app-primary.from-right > div > div.flex-column.flex-center.flex-grow > div > div > p:nth-child(89) > strong > a'));
+	this.copyWordsButton = element(by.buttonText('I\'ve copied it somewhere safe'));
+	this.accountMenu = element(by.className('sandwich-expando'));
+	this.logoutOption = element(by.css('#app-content > div > div:nth-child(3) > span > div > li:nth-child(3)'));
 
 	this.fillPasswordForm = function(passwordObj) {
 		this.newPasswordField.sendKeys(passwordObj.passwordValue);
 		this.confirmPasswordField.sendKeys(passwordObj.passwordValue);
 		this.createButton.click();
+	};
+
+	this.logOut = function(passwordObj) {
+		this.accountMenu.click();
+		basePage.isVisible(this.logoutOption), basePage.timeout.xxl;
+		this.logoutOption.click();
+		basePage.isVisible(this.newPasswordField), basePage.timeout.xxl;
 	};
 };
 
